3a2f3dec10c587a7e38a65a789588cdb56076b36
Alexey-t <support@uvviewsoft.com>
2019-07-13 12:47:53 +0300

    optim

--- __init__.py
+++ __init__.py
@@ -1,8 +1,4 @@
-import sys
-import os
-
-sys.path.append(os.path.dirname(__file__))
-from cssprefixer2 import process
+from .cssprefixer2 import process
 
 def do_format(text):
     return process(text)
